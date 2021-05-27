c92cd9b3e624102f68bd9764b4575b5b5fb4e22e
Masahiro Oono <words.ssp@gmail.com>
2021-05-27 22:10:04 +0900

    Change variable assignment
    Change to use IA32 Machine Language.

--- main.c
+++ main.c
@@ -892,7 +892,7 @@ int compile(String sourceCode)
     int *curBlock = &blockInfo[blockDepth];
     int e0 = 0, e2 = 0;
     if (match(1, "!!*0 = !!*1;", pc)) {
-      putIc(OpCpy, &vars[tc[wpc[0]]], &vars[tc[wpc[1]]], 0, 0);
+      putIcX86("8b_%1m0; 89_%0m0;", &vars[tc[wpc[0]]], &vars[tc[wpc[1]]], 0, 0);
     }
     else if (match(10, "!!*0 = !!*1 + 1; if (!!*2 < !!*3) goto !!*4;", pc) && tc[wpc[0]] == tc[wpc[1]] && tc[wpc[0]] == tc[wpc[2]]) {
       putIc(OpLop, &vars[tc[wpc[4]]], &vars[tc[wpc[0]]], &vars[tc[wpc[3]]], 0);
